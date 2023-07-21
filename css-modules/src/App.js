425a362edac1e68ce114f53e4a0b0d2697438d67
javeria2108 <javeriazh@gmail.com>
2023-07-21 15:13:09 +0500

    initial commit

---
+++ css-modules/src/App.js
@@ -0,0 +1,12 @@
+import Content from "./Components/Content";
+import Header from "./Components/Header";
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+     <Content/>
+    </div>
+  );
+}
+
+export default App;
